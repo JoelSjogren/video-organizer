d57cfae2bb42abb9020350461a25a989bb5de8bb
Joel Sjögren <joelsjogren.wii@gmail.com>
2013-08-07 13:00:28 +0200

    fix .gitignore

---
+++ src/build_no.h
@@ -0,0 +1,8 @@
+/**********************************
+*  help.h                         *
+*                                 *
+*  Declares build number. (-v)    *
+**********************************/
+#pragma once
+extern unsigned char build_no_txt[];
+extern unsigned int build_no_txt_len;
